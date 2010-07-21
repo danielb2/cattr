c4983c747926c1ce1841cfc6044e12ce59913359
Daniel Bretoi <daniel@netwalk.org>
2010-07-20 20:30:26 -0700

    added seperate cattr_reader and cattr_writer

--- lib/cattr.rb
+++ lib/cattr.rb
@@ -1,13 +1,25 @@
 class Class
   def cattr_accessor(*meths)
-    meths.each do |meth|
-      class_variable_set "@@#{meth}", nil
-      self.class.send(:define_method, meth) { class_variable_get("@@#{meth}") }
-      self.class.send(:define_method, :"#{meth}=") { |arg| class_variable_set("@@#{meth}", arg) }
-    end
+    cattr_writer(*meths)
+    cattr_reader(*meths)
   end
   def cattr_reader(*meths)
+    meths.each do |meth|
+      self.class.send(:define_method, meth) { class_variable_get("@@#{meth}") }
+    end
   end
   def cattr_writer(*meths)
+    meths.each do |meth|
+      self.class.send(:define_method, :"#{meth}=") do |arg|
+        # if we can get the class variable it means it exists so
+        # we don't want to overwrite it.
+        begin
+          class_variable_get("@@#{meth}")
+        rescue NameError
+          # if we get a nameerror, then it didn't exist so let's set it
+          class_variable_set("@@#{meth}", arg)
+        end
+      end
+    end
   end
 end
--- test/test_cattr.rb
+++ test/test_cattr.rb
@@ -2,10 +2,32 @@ require 'helper'
 
 class TestCattr < Test::Unit::TestCase
   should "set and get class attributes" do
-    class Foo
-      cattr_accessor :bob
+    class Moo
+      cattr_accessor :cow
     end
-    Foo.bob = 'test'
-    assert('test',Foo.bob)
+    Moo.cow = 'test'
+    assert('test',Moo.cow)
+  end
+
+  should "create class setter" do
+    class Moo
+      cattr_writer :cow
+      def self.get
+        @@cow
+      end
+    end
+    Moo.cow = 'test'
+    assert('test',Moo.get)
+  end
+
+  should "create class reder" do
+    class Moo
+      cattr_reader :cow
+      def self.get=(arg)
+        @@cow = arg
+      end
+    end
+    Moo.get = 'test'
+    assert('test',Moo.cow)
   end
 end
